16c1e15c14bb20f614a331dd0a915250a04087cf
Buto Ka <you@example.com>
2022-07-28 14:12:57 +0700

    Update layout admin lần 2 :v

--- testAdmin/asset/adminjs/admin-app.js
+++ testAdmin/asset/adminjs/admin-app.js
@@ -3,17 +3,28 @@ var app= angular.module('admin-app',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider.
     when("/",{
-        templateUrl: "/page/home/index.html"
-        // controller: "account"
+        templateUrl: "/page/home/index.html",
+        controller: "index"
     }).
     when("/account-form",{
         templateUrl: "/page/account-manager/account-form.html",
-        controller: "account"
+        controller: "account-form"
     })
     .when("/account-list",{
         templateUrl: "/page/account-manager/account-list.html",
-        controller: "account"
+        controller: "account-list"
     }).otherwise({
         template:"<h1 class='text-center'>Home page</h1>"
     })
 });
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.page = {
+        setTitle: function(title) {
+            this.title =  'DTNsBike |' + title;
+        }
+    }
+
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+        $rootScope.page.setTitle(current.$$route.title || ' Trang quản trị');
+    });
+}]);
